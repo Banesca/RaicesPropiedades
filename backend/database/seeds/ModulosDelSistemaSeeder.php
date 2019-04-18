b5c5c67ff054d4129764dcff8676d04910ba4c44
Alejandro Parrilla <alecortez240192@gmail.com>
2019-04-17 21:32:58 -0400

    Update ModulosDelSistemaSeeder.php

--- backend/database/seeds/ModulosDelSistemaSeeder.php
+++ backend/database/seeds/ModulosDelSistemaSeeder.php
@@ -24,5 +24,53 @@ class ModulosDelSistemaSeeder extends Seeder {
             'url'         => 'www.google.co.ve',
             'iconomodulo' => '',
         ]);
+
+        \App\Modulo::create([
+            'idM'         => 3,
+            'modulo'      => 'Sucursales',
+            'descripcion' => 'Gestion de Sucursales',
+            'url'         => 'www.google.co.ve',
+            'iconomodulo' => '',
+        ]);
+
+        \App\Modulo::create([
+            'idM'         => 4,
+            'modulo'      => 'Editar Perfil',
+            'descripcion' => 'Gestion del Perfil',
+            'url'         => 'www.google.co.ve',
+            'iconomodulo' => '',
+        ]);
+
+        \App\Modulo::create([
+            'idM'         => 5,
+            'modulo'      => 'Config General',
+            'descripcion' => 'Gestion Config General',
+            'url'         => 'www.google.co.ve',
+            'iconomodulo' => '',
+        ]);
+
+        \App\Modulo::create([
+            'idM'         => 6,
+            'modulo'      => 'Envio de Email',
+            'descripcion' => 'Gestion del Email',
+            'url'         => 'www.google.co.ve',
+            'iconomodulo' => '',
+        ]);
+
+        \App\Modulo::create([
+            'idM'         => 7,
+            'modulo'      => 'Galeria del Home',
+            'descripcion' => 'Gestion de Galeria del Home',
+            'url'         => 'www.google.co.ve',
+            'iconomodulo' => '',
+        ]);
+
+        \App\Modulo::create([
+            'idM'         => 8,
+            'modulo'      => 'Tasación',
+            'descripcion' => 'Gestion de Tasación',
+            'url'         => 'www.google.co.ve',
+            'iconomodulo' => '',
+        ]);
     }
 }
