fbda6fa28cb618aae6a92766aa3186c2c861fcca
Wladimir Hernandez <wladimirhernandezdev@gmail.com>
2019-05-15 13:02:57 -0400

    prod

---
+++ frontend/src/app/modules/offices/offices.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OfficeComponent } from './office/office.component';
+
+@NgModule({
+  declarations: [OfficeComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class OfficesModule { }
