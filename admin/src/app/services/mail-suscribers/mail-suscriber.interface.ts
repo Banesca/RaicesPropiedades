e1c3bc6d9b119f81fc62f1567aeab68112379819
Wladimir Hernández <wladimirhernandezdev@gmail.com>
2019-06-04 17:33:37 -0400

    stage

--- admin/src/app/services/mail-suscribers/mail-suscriber.interface.ts
+++ admin/src/app/services/mail-suscribers/mail-suscriber.interface.ts
@@ -21,3 +21,27 @@ export interface IMailSuscriberRs {
     }
   }
   
+  
+  export interface ISuscriberRs {
+    msj: string;
+    suscripcion: ISuscriber[];
+  }
+  
+  export interface ISuscriber {
+    idSuscripcionUser: number;
+    titulo: string;
+    descripcion: string;
+    estado?: string;
+  }
+
+  export class Suscriber {
+    static empy() {
+      return {
+        idSuscripcionUser: null,
+        titulo: '',
+        descripcion: '',
+        estado: ''
+      } as IMailSuscriber;
+    }
+  }
+  
