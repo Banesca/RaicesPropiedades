25180a733f62a82f4113b2666d53ef1325297dd2
Alejandro Parrilla <alecortez240192@gmail.com>
2019-06-07 22:29:58 -0400

    server

--- admin/src/app/component/suscribers/suscribers.component.ts
+++ admin/src/app/component/suscribers/suscribers.component.ts
@@ -117,7 +117,6 @@ export class SuscribersComponent implements OnInit {
       });
   }
 
-*/
   guardar() {
     this.mCategoriasSelect = this.mForma.controls["email"].value;
     this.mLoading = true;
