252503898f13d7b389f42ec08a6ac42d39ce9ef7
Breiddy Monterrey <breiddypreteroti@gmail.com>
2019-06-12 20:59:44 -0400

    Arreglo ortografia

--- backend/app/Http/Controllers/GaleriaController.php
+++ backend/app/Http/Controllers/GaleriaController.php
@@ -42,7 +42,7 @@ class GaleriaController extends Controller {
         }else{
             $response = [
                 'msj'        => 'Galeria',
-                'suculsales' => $galeria,
+                'Galeria' => $galeria,
             ];
         }
        
