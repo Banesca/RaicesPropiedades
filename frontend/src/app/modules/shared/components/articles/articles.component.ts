0c45431dc0d5905444cc09d49104465a1f84cb7a
Wladimir Hernandez <wladimirhernandezdev@gmail.com>
2019-07-09 10:02:06 -0400

    filtros del buscador

--- frontend/src/app/modules/shared/components/articles/articles.component.ts
+++ frontend/src/app/modules/shared/components/articles/articles.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { IArticulo } from "../../../../servicios/interfaces.index";
-import { ArticuloService } from "../../../../servicios/servicios.index";
-
 
 @Component({
   selector: 'app-articles',
@@ -9,64 +6,12 @@ import { ArticuloService } from "../../../../servicios/servicios.index";
   styleUrls: ['./articles.component.scss']
 })
 export class ArticlesComponent implements OnInit {
-  mArticulo: IArticulo[];
-  selectedCategory: string = 'Prgramación';
 
-  constructor(private _ArticuloService: ArticuloService) { 
+  constructor() { 
 
-    this.mArticulo;
-    this.getAll();
   }
 
   ngOnInit() {
   }
 
-  getAll() {
-    this._ArticuloService
-      .All()
-        .then(data => {
-        this.mArticulo = data.result;
-        console.log(this.mArticulo);
-
-      })
-      .catch(error => {
-        console.log(this.mArticulo);
-      });
-  }
-  
-
 }
-/*import { Component, OnInit, Input } from '@angular/core';
-import { IArticulo } from "../../../../servicios/interfaces.index";
-import { ArticuloService } from "../../../../servicios/servicios.index";
-import { ActivatedRoute } from '@angular/router';
-
-
-
-@Component({
-  selector: 'app-articles',
-  templateUrl: './articles.component.html',
-  styleUrls: ['./articles.component.scss']
-})
-export class ArticlesComponent implements OnInit {
-  
-  @Input() article: IArticulo;
-
-  constructor(    private route: ActivatedRoute,
-    private _ArticuloService: ArticuloService) { 
-    this.article;
-  }
-
-  ngOnInit(): void {
-    this.getArticle();
-  }
-
-
-  getArticle(): void {
-    const id  = +this.route.snapshot.paramMap.get('id');
-    this._ArticuloService.All(id)
-      .subscribe(article => this.article = article);
-  }
-
-
-}*/
