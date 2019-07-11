f7bdfaed1eb4ce6a42f4eb4f05b535ba117d4396
Alejandro Parrilla <alecortez240192@gmail.com>
2019-07-10 21:30:01 -0400

    mod en back

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -155,6 +155,8 @@ Route::group([ 'prefix' => 'v1', 'middleware' => 'cors' ], function() {
     Route::get('listarPropiedades/{idPropiedad}','Ficha123Controller@listar');
     Route::get('listarPropiedades','Ficha123Controller@listarTodo');
 
+    //Route::post('buscadorGeneral','Buscador');
+
 
 
 });
