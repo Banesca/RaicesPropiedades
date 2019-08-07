2ea6502add9b39684cf0dba353840e522c22e17c
Alejandro Parrilla <alecortez240192@gmail.com>
2019-08-07 09:42:25 -0400

    mejorando el buscador general

--- backend/app/Http/Controllers/BuscadorDePropiedadesController.php
+++ backend/app/Http/Controllers/BuscadorDePropiedadesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Barrio;
 use App\Ficha123;
+use App\Propiedad;
 use App\Region;
 use Illuminate\Http\Request;
 
@@ -11,12 +12,13 @@ class BuscadorDePropiedadesController extends Controller {
 
     public function buscarGeneral(Request $request) {
 
+
         $idTipoOperaion  = @$request->idTipoOperaion;
         $idTipoPropiedad = @$request->idTipoPropiedad;
         $idMonedas       = @$request->idMonedas;
 
-        $resultadoUnico = Ficha123::with('ficha1','ficha2','ficha3')
-            ->whereHas('ficha1', function($query) use ($idTipoOperaion,$idTipoPropiedad,$idMonedas) {
+        $resultadoUnico = Propiedad::
+            where(function($query) use ($idTipoOperaion,$idTipoPropiedad,$idMonedas) {
                    ! is_null($idTipoOperaion) ? $query->where('fk_idTipoOperaion', $idTipoOperaion) : '';
                    ! is_null($idTipoPropiedad) ? $query->where('fk_tipoPropiedad', $idTipoPropiedad) : '';
                    ! is_null($idMonedas) ? $query->where('fk_idMonedas', $idMonedas) : '';
