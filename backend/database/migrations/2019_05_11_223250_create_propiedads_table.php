a2da28d00ddba2b4a442dc1fcb94ea6cf98eb5e1
Alejandro Parrilla <alecortez240192@gmail.com>
2019-08-10 16:32:48 -0400

    en proceso de construccion

--- backend/database/migrations/2019_05_11_223250_create_propiedads_table.php
+++ backend/database/migrations/2019_05_11_223250_create_propiedads_table.php
@@ -289,23 +289,23 @@ class CreatePropiedadsTable extends Migration
             $table->integer('fk_ficha2')->unsigned()->nullable();
 
 
-            $table->text('Edificio_Servicios.ABL')->nullable();
-            $table->text('Edificio_Servicios.AguaCorriente')->nullable();
-            $table->text('Edificio_Servicios.Bar')->nullable();
-            $table->text('Edificio_Servicios.Calefaccion')->nullable();
-            $table->text('Edificio_Servicios.Cocina')->nullable();
-            $table->text('Edificio_Servicios.Conmutador')->nullable();
-            $table->text('Edificio_Servicios.Electricidad')->nullable();
-            $table->text('Edificio_Servicios.GasEnvasado')->nullable();
-            $table->text('Edificio_Servicios.GasNatural')->nullable();
-            $table->text('Edificio_Servicios.Internet')->nullable();
-            $table->text('Edificio_Servicios.Lavanderia')->nullable();
-            $table->text('Edificio_Servicios.Refrigeracion')->nullable();
-            $table->text('Edificio_Servicios.Rentas')->nullable();
-            $table->text('Edificio_Servicios.Seguridad')->nullable();
-            $table->text('Edificio_Servicios.Telefono')->nullable();
-            $table->text('Edificio_Servicios.Videocable')->nullable();
-            $table->text('Edificio_Servicios.Vigilancia')->nullable();
+            $table->text('Edificio_Servicios_ABL')->nullable();
+            $table->text('Edificio_Servicios_AguaCorriente')->nullable();
+            $table->text('Edificio_Servicios_Bar')->nullable();
+            $table->text('Edificio_Servicios_Calefaccion')->nullable();
+            $table->text('Edificio_Servicios_Cocina')->nullable();
+            $table->text('Edificio_Servicios_Conmutador')->nullable();
+            $table->text('Edificio_Servicios_Electricidad')->nullable();
+            $table->text('Edificio_Servicios_GasEnvasado')->nullable();
+            $table->text('Edificio_Servicios_GasNatural')->nullable();
+            $table->text('Edificio_Servicios_Internet')->nullable();
+            $table->text('Edificio_Servicios_Lavanderia')->nullable();
+            $table->text('Edificio_Servicios_Refrigeracion')->nullable();
+            $table->text('Edificio_Servicios_Rentas')->nullable();
+            $table->text('Edificio_Servicios_Seguridad')->nullable();
+            $table->text('Edificio_Servicios_Telefono')->nullable();
+            $table->text('Edificio_Servicios_Videocable')->nullable();
+            $table->text('Edificio_Servicios_Vigilancia')->nullable();
 
             $table->integer('fk_Direccion_Region_Id')->unsigned()->nullable();
             $table->integer('fk_TipoUnidadCasa')->unsigned()->nullable();
