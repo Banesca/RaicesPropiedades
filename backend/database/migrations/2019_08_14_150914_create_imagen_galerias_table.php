626b450d0fd95733f29456e06e8b921a77d228ca
Alejandro Parrilla <alecortez240192@gmail.com>
2019-08-14 15:27:10 -0400

    add imagenes to galaria

---
+++ backend/database/migrations/2019_08_14_150914_create_imagen_galerias_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateImagenGaleriasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fk_imagen_galerias', function (Blueprint $table) {
+            $table->increments('idImagenesGaleria');
+            $table->string('imagen');
+            $table->integer('fk_idGaleria')->nullable()->unsigned();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fk_imagen_galerias');
+    }
+}
