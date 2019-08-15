9cf8f99766d92701bbe61aa4f8414d4dbcfa0c75
Alejandro Parrilla <alecortez240192@gmail.com>
2019-08-14 22:08:55 -0400

    nueva de listar todo sin filtro

--- backend/app/Http/Controllers/Ficha123Controller.php
+++ backend/app/Http/Controllers/Ficha123Controller.php
@@ -144,4 +144,74 @@ class Ficha123Controller extends Controller
         }
 
     }
+
+    public function listarTodoSinFiltro()
+    {
+        $ficha123 = Propiedad::with(
+            'TipoPropiedad',
+            'Disposicion',
+            'Estado',
+            'Orientacion',
+            'TipoAcceso',
+            'TipoAscensor',
+            'TipoBalcon',
+            'TipoBano',
+            'TipoCalefaccion',
+            'TipoCampo',
+            'TipoCobertura',
+            'TipoCoche',
+            'TipoCochera',
+            'TipoCosta',
+            'TipoEdificio',
+            'TipoExpensas',
+            'TipoFondoComercio',
+            'TipoFrente',
+            'TipoHotel',
+            'TipoLocal',
+            'TipoPendiente',
+            'TipoPiso',
+            'TipoPorton',
+            'TipoTecho',
+            'TipoTechoIndustrial',
+            'TipoTerreno',
+            'TipoUnidadCasa',
+            'TipoUnidadDepartamento',
+            'Barrio',
+            'Localidad',
+            'Pais',
+            'Partido',
+            'Provincia',
+            'SubBarrio',
+            'Region',
+            'TipoVista',
+            'estadoPublicacion',
+            'tipoOpeacion',
+            'tipoMoneda'
+        )
+            ->get();
+
+        if (!is_null($ficha123)) {
+
+            $ficha123->each(function ($ficha123) {
+                $ficha123->imagenes = [
+                    'imagen1'             => asset('storage\\ficha2\\' . @$ficha123->imagen1),
+                    'imagen2'             => asset('storage\\ficha2\\' . @$ficha123->imagen2),
+                    'imagen3'             => asset('storage\\ficha2\\' . @$ficha123->imagen3),
+                    'imagen4'             => asset('storage\\ficha2\\' . @$ficha123->imagen4),
+                    'imagen5'             => asset('storage\\ficha2\\' . @$ficha123->imagen5),
+                    'imagen6'             => asset('storage\\ficha2\\' . @$ficha123->imagen6),
+                    'imagen7'             => asset('storage\\ficha2\\' . @$ficha123->imagen7),
+                    'imagen_para_galeria' => asset('storage\\ficha2\\' . @$ficha123->imagen_para_galeria),
+                ];
+            });
+
+            return response()->json($ficha123);
+        }else{
+            $response = [
+                'msj'             => 'No existe la propiedad',
+            ];
+            return response()->json($response, 200);
+        }
+
+    }
 }
