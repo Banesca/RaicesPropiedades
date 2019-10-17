ee95722d41f83dac99f6a7c9c700438adbb28633
Alejandro Parrilla <alecortez240192@gmail.com>
2019-10-17 11:48:23 -0400

    activacion de hash en front

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,31 +1,30 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './page/home/home.component';
-import { AboutComponent } from './page/about/about.component';
-import { PropiertysComponent } from './page/propiertys/propiertys.component';
-import { NopagefoundComponent } from './page/nopagefound/nopagefound.component';
+import {NgModule} from "@angular/core";
+import {RouterModule, Routes} from "@angular/router";
+import {HomeComponent} from "./page/home/home.component";
+import {AboutComponent} from "./page/about/about.component";
+import {PropiertysComponent} from "./page/propiertys/propiertys.component";
+import {NopagefoundComponent} from "./page/nopagefound/nopagefound.component";
 
 const routes: Routes = [
-  {path: '', redirectTo:'home' , pathMatch: 'full'},
-  { path: 'home', component: HomeComponent },
-  { path: 'nosotros', component: AboutComponent },
-  { path: 'oportunidades', component: PropiertysComponent },
-  {
-    path: 'buscador',
-    loadChildren: './modules/blog/blog.module#BlogModule'
-  },
-  {
-    path: 'sucursales',
-    loadChildren: './modules/offices/offices.module#OfficesModule'
-  },
-  { path: '**', component: NopagefoundComponent }
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: 'home', component: HomeComponent},
+    {path: 'nosotros', component: AboutComponent},
+    {path: 'oportunidades', component: PropiertysComponent},
+    {
+        path: 'buscador',
+        loadChildren: './modules/blog/blog.module#BlogModule'
+    },
+    {
+        path: 'sucursales',
+        loadChildren: './modules/offices/offices.module#OfficesModule'
+    },
+    {path: '**', component: NopagefoundComponent}
 ];
 
 
-
 @NgModule({
-  imports: [RouterModule.forRoot(routes,
-    {scrollPositionRestoration: 'top'})],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: 'top', useHash: true})],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
