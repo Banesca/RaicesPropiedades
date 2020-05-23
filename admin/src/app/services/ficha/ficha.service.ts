7f8494a87454c619cf520d96e176dfff445f380c
Victor <utrera1695@gmail.com>
2020-05-22 23:15:47 -0400

    correcion de modal y ficha

--- admin/src/app/services/ficha/ficha.service.ts
+++ admin/src/app/services/ficha/ficha.service.ts
@@ -1,57 +1,50 @@
-import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
-import {environment} from '../../../environments/environment';
-import {getHeaders} from '../../misc/Headers';
-import { /*IFichasRs,*/ IFichas} from './ficha.interface';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { environment } from "../../../environments/environment";
+import { getHeaders } from "../../misc/Headers";
+import { /*IFichasRs,*/ IFichas } from "./ficha.interface";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
-
-
 export class FichaService {
+  private cabeceraPrueba = { "Content-Type": "application/json" };
 
-  private cabeceraPrueba = {'Content-Type': 'application/json'};
-
-  private fService = '/api/v1/fichaPropiedad';
+  private fService = "/api/v1/fichaPropiedad";
   //private fService = '/api/auth/fichaPropiedad';
   private fUrl: string;
 
+  constructor(private _HttpClient: HttpClient) {}
 
-
-  constructor( private _HttpClient: HttpClient) {}
-
-  All(): Observable<any>
-  {
-
-    return this._HttpClient.get(environment.apiHost + this.fService + '/getAll',
-    {
-      headers: this.cabeceraPrueba
-      //headers: getHeaders()
-    })
-    .pipe(map(
-      (res) => {
-        return res;
+  All(): Observable<any> {
+    return this._HttpClient
+      .get(environment.apiHost + this.fService + "/getAll", {
+        headers: this.cabeceraPrueba,
+        //headers: getHeaders()
       })
-    );
-
+      .pipe(
+        map((res) => {
+          return res;
+        })
+      );
   }
 
-  goToPdf(id:number){
-    window.open(`${environment.apiHost}/api/v1/fichaPropiedad/pdf/${id}`,'_blank');
-}
+  goToPdf(id: number) {
+    window.open(
+      `${environment.apiHost}/api/v1/fichaPropiedad/pdf/${id}`,
+      "_blank"
+    );
+  }
 
-  agregarFicha(ficha){
-
+  agregarFicha(ficha) {
     return this._HttpClient
       .post<any>(environment.apiHost + this.fService + "/agregar", ficha, {
         headers: new HttpHeaders({
           "Access-Control-Allow-Origin": "*",
-          Authorization: "Bearer " + localStorage.getItem("access_token")
-        })
+          Authorization: "Bearer " + localStorage.getItem("access_token"),
+        }),
       })
       .pipe(
         map((data: any) => {
@@ -59,39 +52,33 @@ export class FichaService {
         })
       )
       .toPromise();
-
   }
 
-  actualizarFicha(ficha: IFichas, fKey: number) {
-    return this._HttpClient.post(
-      environment.apiHost + this.fService + 'editar' + fKey,
-      ficha,
-      {
-        headers: this.cabeceraPrueba
+  actualizarFicha(ficha, fKey) {
+    return this._HttpClient
+      .post(environment.apiHost + this.fService + "/editar/" + fKey, ficha, {
+        headers: this.cabeceraPrueba,
         //headers: getHeaders()
-      }
-    ).pipe(
-      map((data: any) => {
-        return data;
       })
-    ).toPromise();
-  }
-
-
-  eliminarFicha(fKey: number) {
-    return this._HttpClient.delete(
-      environment.apiHost + this.fService + '/borrar/' + fKey,
-        {
-          headers: this.cabeceraPrueba
-          //headers: getHeaders()
-        }
-      ).pipe(
+      .pipe(
         map((data: any) => {
           return data;
         })
-      ).toPromise();
+      )
+      .toPromise();
   }
 
-
-
+  eliminarFicha(fKey: number) {
+    return this._HttpClient
+      .delete(environment.apiHost + this.fService + "/borrar/" + fKey, {
+        headers: this.cabeceraPrueba,
+        //headers: getHeaders()
+      })
+      .pipe(
+        map((data: any) => {
+          return data;
+        })
+      )
+      .toPromise();
+  }
 }
